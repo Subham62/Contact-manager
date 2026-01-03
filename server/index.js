ced4398fafd418e1938c68acd197ecf0e81dadf1
Subham62 <subhamchakraborty2002@gmail.com>
2026-01-03 17:44:07 +0530

    Updation for deployment

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './database/db.js';
 import contactRoutes from './routes/contactRoute.js';
+import path from "path";
 
 dotenv.config();
 
@@ -11,10 +12,17 @@ const app = express();
 // Connect to MongoDB
 connectDB();
 
+const _dirname = path.resolve();
+
 app.use(cors());  
 app.use(express.json()); 
 
 app.use('/api/contacts', contactRoutes);
+
+app.use(express.static(path.join(_dirname, "/client/dist")));
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.resolve(_dirname, "client", "dist", "index.html"));
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
